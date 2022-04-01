274b8500ec9d2eba7737f7e3aa6d0ec3c1686a9c
Your Name <lanre@fordsoft.tech>
2022-04-01 12:12:57 +0100

    first task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NavbarComponent } from './component/navbar/navbar.component';
+import { LoginVerifyComponent } from './component/login-verify/login-verify.component';
+import { RecoveryPasswordComponent } from './component/recovery-password/recovery-password.component';
+import { SignupComponent } from './component/signup/signup.component';
 
 const routes: Routes = [
-  {path:'navbar',component:NavbarComponent},
+  { path: 'signup', component: SignupComponent },
+  { path: 'login_verify', component: LoginVerifyComponent },
+  { path: 'recovery_password', component: RecoveryPasswordComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentComponent } from './component/component.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
+import { SignupComponent } from './component/signup/signup.component';
+import { LoginVerifyComponent } from './component/login-verify/login-verify.component';
+import { RecoveryPasswordComponent } from './component/recovery-password/recovery-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentComponent,
-    NavbarComponent
+    NavbarComponent,
+    SignupComponent,
+    LoginVerifyComponent,
+    RecoveryPasswordComponent
   ],
   imports: [
     BrowserModule,
